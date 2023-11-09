c5e0899e3caa1093511707f171fdf2ee046ff5a9
najmiddin <turgunpolatov3gmail.com>
2023-11-09 22:18:00 +0500

    commit

--- app/adminMainPage/page.jsx
+++ app/adminMainPage/page.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import AdminMainPage from "../../components/AdminMainPage"
 import Navbar from "../../components/Navbar"
+import Image from 'next/image'
+import ITPARK from "../../public/Logo_IT_Park_Uzbekistan.svg.png"
 
 export default function page() {
     return (
@@ -8,6 +10,10 @@ export default function page() {
             <div className='pb-4'>
                 <Navbar />
                 <AdminMainPage />
+                <div className='flex mt-16 gap-1 justify-center items-center'>
+                    <h1 className='text-center text-[16px] poppins'>Web Sayt Chortoq IT Park jamoasi tomonidan tuzildi</h1>
+                    <Image src={ITPARK} alt='Image' width={50} height={10} />
+                </div>
             </div>
         </>
     )
